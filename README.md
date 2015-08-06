29a0eaa487c962315c12b5b24af64c74cee68a61
Narno <arnaud@ligny.org>
2015-08-06 11:25:25 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Open New Tab After Current Tab
+==============================
+
+_Open New Tab After Current Tab_ is a Google Chrome extension that opens new tab after the current tab, instead of last position.
+
+Installation
+------------
+
+### Manually
+
+1. Download [the lastest release](https://github.com/Narno/Open-New-Tab-After-Current-Tab/releases)
+2. Open the extensions page of your browser
+3. Drag & drop the _.crx_ file in that tab
+
+### From [Chrome Web Store](https://chrome.google.com/webstore/)  
+[![Facebook Notifier available in the Chrome Web Store](https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_206x58.png)](https://chrome.google.com/webstore/
---
+++ src/background.js
@@ -0,0 +1,56 @@
+(function () {
+  "use strict";
+
+  var currentIndex;
+
+  function getCurrentActiveTab() {
+    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+      console.log('chrome.tabs.query - index: ' + tabs[0].index); // debug
+      currentIndex = tabs[0].index;
+    });
+    // fallback
+    if (Number.isInteger(currentIndex)) {
+      // last position (default behavior)
+      chrome.tabs.query({currentWindow: true}, function (tabs) {
+        console.log('chrome.runtime.onInstalled - tabs.length: ' + tabs.length); // debug
+        currentIndex = tabs.length;
+      });
+    }
+  }
+
+  /**
+   * Remember the current tab index
+   */
+  // on extension install/update
+  chrome.runtime.onInstalled.addListener(function(details) {
+    getCurrentActiveTab();
+  });
+  // on extension enabled
+  chrome.management.onEnabled.addListener(function(ExtensionInfo) {
+    getCurrentActiveTab();
+  });
+  // on window focused
+  chrome.windows.onFocusChanged.addListener(function(windowId) {
+    chrome.tabs.getSelected(chrome.tabs.windowId, function(tab) {
+      console.log('chrome.windows.onFocusChanged - tab.index: ' + tab.index); // debug
+      currentIndex = tab.index;
+    });
+  });
+  // on tab activated
+  chrome.tabs.onActivated.addListener(function(activeInfo) {
+    chrome.tabs.get(activeInfo.tabId, function(tab) {
+      console.log('chrome.tabs.onActivated - tab.index: ' + tab.index); // debug
+      currentIndex = tab.index;
+    });
+  });
+
+  /**
+   * Move new tab after the current
+   */
+  chrome.tabs.onCreated.addListener(function(tab) {
+    console.log('chrome.tabs.onCreated - currentIndex: ' + currentIndex); // debug
+    chrome.tabs.move(tab.id, {
+      index: currentIndex + 1
+    });
+  });
+})();
