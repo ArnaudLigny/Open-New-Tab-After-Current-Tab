98a5f0793126c0723e2a3bd5dc97760c2cdddf1e
Narno <arnaud@ligny.org>
2015-08-09 03:43:02 +0200

    Fine tuning!

--- src/background.js
+++ src/background.js
@@ -1,21 +1,23 @@
 (function () {
   "use strict";
 
-  var currentIndex;
+  var currentIndex = [];
 
   function getCurrentActiveTab() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
       console.log(tabs[0].windowId + ' - chrome.tabs.query - index: ' + tabs[0].index); // debug
-      currentIndex = tabs[0].index;
+      currentIndex[tabs[0].windowId] = tabs[0].index;
     });
     // fallback
-    if (Number.isInteger(currentIndex)) {
-      // last position (default behavior)
-      chrome.tabs.query({currentWindow: true}, function (tabs) {
-        console.log(tabs[0].windowId + ' - chrome.runtime.onInstalled - tabs.length: ' + tabs.length); // debug
-        currentIndex = tabs.length;
-      });
-    }
+    chrome.windows.getCurrent(function(window) {
+      if (!Number.isInteger(currentIndex[window.id])) {
+        // last position (default behavior)
+        chrome.tabs.query({currentWindow: true}, function (tabs) {
+          console.log(tabs[0].windowId + ' - fallback - tabs.length: ' + tabs.length); // debug
+          currentIndex[tabs[0].windowId] = tabs.length;
+        });
+      }
+    });
   }
 
   /**
@@ -23,11 +25,17 @@
    */
   // on extension install/update
   chrome.runtime.onInstalled.addListener(function(details) {
-    getCurrentActiveTab();
+    if (details.reason == 'install' || details.reason == 'update') {
+      console.log('chrome.runtime.onInstalled'); // debug
+      getCurrentActiveTab();
+    }
   });
   // on extension enabled
   chrome.management.onEnabled.addListener(function(ExtensionInfo) {
-    getCurrentActiveTab();
+    if (ExtensionInfo.id == chrome.runtime.id) {
+      console.log('chrome.management.onEnabled'); // debug
+      getCurrentActiveTab();
+    }
   });
   // on window focused
   chrome.windows.onFocusChanged.addListener(function(windowId) {
@@ -35,7 +43,7 @@
     if (windowId != -1 && windowId != -2) {
       chrome.tabs.getSelected(chrome.tabs.windowId, function(tab) {
         console.log(windowId + ' - chrome.windows.onFocusChanged - tab.index: ' + tab.index); // debug
-        currentIndex = tab.index;
+        currentIndex[windowId] = tab.index;
       });
     }
   });
@@ -43,23 +51,23 @@
   chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
       console.log(tab.windowId + ' - chrome.tabs.onActivated - tab.index: ' + tab.index); // debug
-      currentIndex = tab.index;
+      currentIndex[tab.windowId] = tab.index;
     });
   });
   // on tab manually moved
   chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {
     console.log(moveInfo.windowId + ' - chrome.tabs.onMoved - moveInfo.toIndex: ' + moveInfo.toIndex); // debug
-    currentIndex = moveInfo.toIndex;
+    currentIndex[moveInfo.windowId] = moveInfo.toIndex;
   });
 
   /**
    * Move new tab after the current
    */
   chrome.tabs.onCreated.addListener(function(tab) {
-    console.log(tab.windowId + ' - chrome.tabs.onCreated - currentIndex: ' + currentIndex); // debug
-    if (Number.isInteger(currentIndex)) {
+    console.log(tab.windowId + ' - chrome.tabs.onCreated - currentIndex: ' + currentIndex[tab.windowId]); // debug
+    if (Number.isInteger(currentIndex[tab.windowId])) {
       chrome.tabs.move(tab.id, {
-        index: currentIndex + 1
+        index: currentIndex[tab.windowId] + 1
       });
     }
   });
