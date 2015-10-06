2ddc74fcdfb8393dc7cecd7a7062b12e4825370a
Narno <arnaud@ligny.org>
2015-10-06 22:57:04 +0200

    Rollback to "onCreated" event instead of "onUpdated" and some fixes
    I should test on Mac OS: It seems the "onCreated" event returns an empty
    index

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@
 
   function getCurrentActiveTab() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-      console.debug(tabs[0].windowId + ' - chrome.tabs.query - index: ' + tabs[0].index); // debug
+      console.debug(tabs[0].windowId + ' - chrome.tabs.query - set currentIndex = index: ' + tabs[0].index); // debug
       currentIndex[tabs[0].windowId] = tabs[0].index;
     });
     // fallback
@@ -13,11 +13,33 @@
       if (!Number.isInteger(currentIndex[window.id])) {
         // last position (default behavior)
         chrome.tabs.query({currentWindow: true}, function (tabs) {
-          console.debug(tabs[0].windowId + ' - fallback - tabs.length: ' + tabs.length); // debug
+          console.debug(tabs[0].windowId + ' - fallback - set currentIndex = tabs.length: ' + tabs.length); // debug
           currentIndex[tabs[0].windowId] = tabs.length;
         });
       }
     });
+  }
+
+  function moveIt(tab, event) {
+    console.debug('----');
+    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get new tab.index: ' + tab.index + ' (' + tab.title + ')'); // debug
+    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get currentIndex: ' + currentIndex[tab.windowId]); // debug
+    if (Number.isInteger(currentIndex[tab.windowId])) {
+      var moveToIndex = currentIndex[tab.windowId] + 1;
+      if (tab.index == moveToIndex) {
+        console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - tab.index: ' + tab.index + ' == moveToIndex: ' + moveToIndex + ' (nothing to do)'); // debug
+        return;
+      }
+      console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - move to: ' + moveToIndex);
+      chrome.tabs.move(tab.id, {
+        index: moveToIndex
+      });
+    }
+  }
+
+  var eventOnMoved = function (tabId, moveInfo) {
+    console.debug(moveInfo.windowId + ' - chrome.tabs.onMoved - set currentIndex = moveInfo.toIndex: ' + moveInfo.toIndex); // debug
+    currentIndex[moveInfo.windowId] = moveInfo.toIndex;
   }
 
   /**
@@ -42,7 +64,7 @@
     // https://developer.chrome.com/extensions/windows#property-WINDOW_ID_NONE
     if (windowId != -1 && windowId != -2) {
       chrome.tabs.getSelected(chrome.tabs.windowId, function(tab) {
-        console.debug(windowId + ' - chrome.windows.onFocusChanged - tab.index: ' + tab.index); // debug
+        console.debug(windowId + ' - chrome.windows.onFocusChanged - set currentIndex = tab.index: ' + tab.index); // debug
         currentIndex[windowId] = tab.index;
       });
     }
@@ -50,37 +72,26 @@
   // on tab activated
   chrome.tabs.onActivated.addListener(function(activeInfo) {
     chrome.tabs.get(activeInfo.tabId, function(tab) {
-      console.debug(tab.windowId + ' - chrome.tabs.onActivated - tab.index: ' + tab.index); // debug
+      console.debug(tab.windowId + ' - chrome.tabs.onActivated - set currentIndex = tab.index: ' + tab.index); // debug
       currentIndex[tab.windowId] = tab.index;
     });
   });
   // on tab manually moved
-  chrome.tabs.onMoved.addListener(function(tabId, moveInfo) {
-    console.debug(moveInfo.windowId + ' - chrome.tabs.onMoved - moveInfo.toIndex: ' + moveInfo.toIndex); // debug
-    currentIndex[moveInfo.windowId] = moveInfo.toIndex;
-  });
+  chrome.tabs.onMoved.addListener(eventOnMoved);
 
   /**
    * Move new tab after the current
    */
   // on created
   chrome.tabs.onCreated.addListener(function(tab) {
-    console.debug(tab.windowId + ' - chrome.tabs.onCreated - tab.index: ' + tab.index); // debug
+    chrome.tabs.onMoved.removeListener(eventOnMoved);
+    moveIt(tab, 'onCreated'); // Windows: OK / Mac OS: ?
+    chrome.tabs.onMoved.addListener(eventOnMoved);
   });
   // on updated
+  /*
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    console.debug(tab.windowId + ' - chrome.tabs.onUpdated - tab.index: ' + tab.index); // debug
-    console.debug('tab.title: ' + tab.title);
-    if (Number.isInteger(currentIndex[tab.windowId])) {
-      console.debug(tab.windowId + ' - chrome.tabs.onUpdated - currentIndex: ' + currentIndex[tab.windowId]); // debug
-      var moveToIndex = currentIndex[tab.windowId] + 1;
-      console.debug('Is tab.index: ' + tab.index + ' + 1 != ' + 'moveToIndex: ' + moveToIndex + ' ?'); // debug
-      if (tab.index + 1 != moveToIndex) {
-        console.debug('Move it to: ' + moveToIndex);
-        chrome.tabs.move(tab.id, {
-          index: moveToIndex
-        });
-      }
-    }
+    moveIt(tab, 'onUpdated');
   });
+  */
 })();
