a71f9547d49a0f259d8d79947736ac1ead485417
Narno <arnaud@ligny.org>
2015-10-07 23:41:34 +0200

    Cleaning code

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@
 
   function getCurrentActiveTab() {
     chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-      console.debug(tabs[0].windowId + ' - chrome.tabs.query - set currentIndex = index: ' + tabs[0].index); // debug
+      console.debug(tabs[0].windowId + ' - chrome.tabs.query - set currentIndex = index: ' + tabs[0].index);
       currentIndex[tabs[0].windowId] = tabs[0].index;
     });
     // fallback
@@ -13,7 +13,7 @@
       if (!Number.isInteger(currentIndex[window.id])) {
         // last position (default behavior)
         chrome.tabs.query({currentWindow: true}, function (tabs) {
-          console.debug(tabs[0].windowId + ' - fallback - set currentIndex = tabs.length: ' + tabs.length); // debug
+          console.debug(tabs[0].windowId + ' - fallback - set currentIndex = tabs.length: ' + tabs.length);
           currentIndex[tabs[0].windowId] = tabs.length;
         });
       }
@@ -21,13 +21,12 @@
   }
 
   function moveIt(tab, event) {
-    console.debug('tab created');
-    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get new tab.index: ' + tab.index + ' (' + tab.title + ')'); // debug
-    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get currentIndex: ' + currentIndex[tab.windowId]); // debug
+    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get new tab.index: ' + tab.index);
+    console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - get currentIndex: ' + currentIndex[tab.windowId]);
     if (Number.isInteger(currentIndex[tab.windowId])) {
       var moveToIndex = currentIndex[tab.windowId] + 1;
       if (tab.index == moveToIndex) {
-        console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - tab.index: ' + tab.index + ' == moveToIndex: ' + moveToIndex + ' (nothing to do)'); // debug
+        console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - tab.index: ' + tab.index + ' == moveToIndex: ' + moveToIndex + ' (nothing to do)');
         return;
       }
       console.debug(tab.windowId + ' - chrome.tabs.' + event + ' - move to: ' + moveToIndex);
@@ -35,11 +34,10 @@
         index: moveToIndex
       });
     }
-    console.debug('/ tab created');
   }
 
   var eventOnMoved = function (tabId, moveInfo) {
-    console.debug(moveInfo.windowId + ' - chrome.tabs.onMoved - set currentIndex = moveInfo.toIndex: ' + moveInfo.toIndex); // debug
+    console.debug(moveInfo.windowId + ' - chrome.tabs.onMoved - set currentIndex = moveInfo.toIndex: ' + moveInfo.toIndex);
     currentIndex[moveInfo.windowId] = moveInfo.toIndex;
   }
 
@@ -49,14 +47,14 @@
   // on extension install/update
   chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == 'install' || details.reason == 'update') {
-      console.debug('chrome.runtime.onInstalled'); // debug
+      console.debug('chrome.runtime.onInstalled');
       getCurrentActiveTab();
     }
   });
   // on extension enabled
   chrome.management.onEnabled.addListener(function(ExtensionInfo) {
     if (ExtensionInfo.id == chrome.runtime.id) {
-      console.debug('chrome.management.onEnabled'); // debug
+      console.debug('chrome.management.onEnabled');
       getCurrentActiveTab();
     }
   });
@@ -65,7 +63,7 @@
     // https://developer.chrome.com/extensions/windows#property-WINDOW_ID_NONE
     if (windowId != -1 && windowId != -2) {
       chrome.tabs.getSelected(chrome.tabs.windowId, function(tab) {
-        console.debug(windowId + ' - chrome.windows.onFocusChanged - set currentIndex = tab.index: ' + tab.index); // debug
+        console.debug(windowId + ' - chrome.windows.onFocusChanged - set currentIndex = tab.index: ' + tab.index);
         currentIndex[windowId] = tab.index;
       });
     }
@@ -76,7 +74,7 @@
       console.log(chrome.runtime.lastError.message);
     } else {
       chrome.tabs.get(activeInfo.tabId, function(tab) {
-        console.debug(tab.windowId + ' - chrome.tabs.onActivated - set currentIndex = tab.index: ' + tab.index); // debug
+        console.debug(tab.windowId + ' - chrome.tabs.onActivated - set currentIndex = tab.index: ' + tab.index);
         currentIndex[tab.windowId] = tab.index;
       });
     }
@@ -90,13 +88,7 @@
   // on created
   chrome.tabs.onCreated.addListener(function(tab) {
     chrome.tabs.onMoved.removeListener(eventOnMoved);
-    moveIt(tab, 'onCreated'); // Windows: OK / Mac OS: ?
+    moveIt(tab, 'onCreated');
     chrome.tabs.onMoved.addListener(eventOnMoved);
   });
-  // on updated
-  /*
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    moveIt(tab, 'onUpdated');
-  });
-  */
 })();
