bd2cec1b5ae37fc83a66d183b1adf84c13e06db1
Arnaud Ligny <arnaud@ligny.fr>
2026-05-04 10:55:30 +0200

    Use querySelector instead of getElementById

--- src/options.js
+++ src/options.js
@@ -1,17 +1,17 @@
 const manifest = chrome.runtime.getManifest();
 const i18n = chrome.i18n.getMessage.bind(chrome.i18n);
 
-document.getElementById('page-title').textContent = i18n('optionsAboutTitle');
-/*document.getElementById('ext-name').textContent = manifest.name;*/
-document.getElementById('ext-description').textContent = i18n('extDescription');
-document.getElementById('ext-version').textContent = manifest.version;
-document.getElementById('ext-author').textContent = manifest.author;
+document.querySelector('#page-title').textContent = i18n('optionsAboutTitle');
+/* Commented: document.querySelector('#ext-name').textContent = manifest.name; */
+document.querySelector('#ext-description').textContent = i18n('extDescription');
+document.querySelector('#ext-version').textContent = manifest.version;
+document.querySelector('#ext-author').textContent = manifest.author;
 
-const homepageLink = document.getElementById('ext-homepage');
+const homepageLink = document.querySelector('#ext-homepage');
 homepageLink.href = manifest.homepage_url;
 homepageLink.textContent = manifest.homepage_url;
 
-document.getElementById('label-version').textContent = i18n('optionsVersion');
-document.getElementById('label-author').textContent = i18n('optionsAuthor');
-document.getElementById('label-website').textContent = i18n('optionsWebsite');
-document.getElementById('label-source').textContent = i18n('optionsSourceCode');
+document.querySelector('#label-version').textContent = i18n('optionsVersion');
+document.querySelector('#label-author').textContent = i18n('optionsAuthor');
+document.querySelector('#label-website').textContent = i18n('optionsWebsite');
+document.querySelector('#label-source').textContent = i18n('optionsSourceCode');
